a29d3d5de72491e930cf9c06cdcfe2892123f702
jnhan <jnhan@student.ubc.ca>
2022-03-15 13:46:47 -0700

    Created UI interface for user input

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import React, {useState} from 'react';
 
-function ExpenseItem({date, title, amount}) {
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
     const clickHandler = () => {
-        console.log('Clicked!!!')
+        setTitle("updated");
+        console.log(title);
     };
 
     return (
         <Card className='expense-item'>
-            <ExpenseDate date={date}></ExpenseDate>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{title}</h2>
-                <div className='expense-item__price'>${amount}</div>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
         </Card>
